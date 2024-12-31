81a22b18d854446930f062731a10e04e7530eb99
soham-shelke <soham3105@gmail.com>
2024-12-31 09:57:07 +0530

    EmptySquare.java

--- EmptySquare.java
+++ EmptySquare.java
@@ -10,7 +10,7 @@ public class EmptySquare {
         int columns = sc.nextInt();
         for(int i = 0; i<row; i++){
             for(int j = 0; j<columns ; j++){
-                if(i==0 | i==row-1 | j==0 | j==columns-1){
+                if(i==0 || i==row-1 || j==0 || j==columns-1){
                  System.out.print("*");}
                 else{
                     System.out.print(" ");
