f27c688d136214c4ecb573683e862380ab5c41b0
soham-shelke <soham3105@gmail.com>
2024-12-31 21:19:38 +0530

    NumberPyramid.java

--- NumberPyramid.java
+++ NumberPyramid.java
@@ -6,7 +6,7 @@ public class Parellelogram {
        System.out.print("Enter the number of rows: ");
        int rows= sc.nextInt();
     for(int i = 1; i<=rows; i++ ){
-        for(int j = rows-i; j>=0 ;j--){
+        for(int j = rows-i; j>=1 ;j--){
             System.out.print(" ");
         }
         for(int j = 1; j<=i; j++){
