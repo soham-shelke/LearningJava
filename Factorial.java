9e96021ec0abc35b9910f0616010003a6a424fb8
soham-shelke <soham3105@gmail.com>
2025-03-03 21:38:02 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial {
+    public static int factorial(int n) {
+        return (n == 0 || n == 1) ? 1 : n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is " + factorial(num));
+    }
+}
